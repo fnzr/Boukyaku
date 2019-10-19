c4e419f2478f4bbc58eaf6b6ac7fd6096230bc2e
fnzr <5471818+fnzr@users.noreply.github.com>
2019-10-18 23:30:47 -0300

    Proxy image requests, add secrets

--- src/server.ts
+++ src/server.ts
@@ -4,12 +4,33 @@ import express, { Request, Response } from 'express';
 import { listCovers, countGalleries } from '@database';
 import { downloadGallery } from '@extractor/exhentai';
 import logger from '@logger';
-
+import proxy from 'express-http-proxy';
 
 function startServer(port?: number) {
     const app = express()
 
     app.use(express.json())
+
+    app.use('/vault/:gallery/:filename$',
+        proxy(process.env.VAULT_URL!, {
+            proxyReqPathResolver: req => {
+                const g = req.params.gallery;
+                const f = req.params.filename;
+                return `/${g}/${f}`;
+            }
+        })
+    )
+
+    app.use('/vault/:options/:gallery/:filename',
+        proxy(process.env.IMAGEPROXY_URL!, {
+            proxyReqPathResolver: req => {
+                const g = req.params.gallery;
+                const f = req.params.filename;
+                const o = req.params.options;
+                return `/${o}/${process.env.VAULT_URL!}/${g}/${f}`;
+            }
+        })
+    )
 
     app.post('/covers', async (req: Request, res: Response) => {
         const offset = "offset" in req.body ? req.body["offset"] : 0;
